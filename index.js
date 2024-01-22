c6cffc4b98a3769a8903aea5725017a50a93c9a4
yuzbamaria <54204342+yuzbamaria@users.noreply.github.com>
2024-01-22 13:04:10 +0000

    added files

--- index.js
+++ index.js
@@ -12,4 +12,9 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
-
+// Steps 
+// 1. npm init -y – to initialise package.json file
+// 2. npm i inquirer@6.3.1 - to install inquirer
+// 3. npm i jest 
+// 4. uptade the file package-json line 10 to: "test": "jest"
+// 5. create a file .gitignore and write in it: node_modules
